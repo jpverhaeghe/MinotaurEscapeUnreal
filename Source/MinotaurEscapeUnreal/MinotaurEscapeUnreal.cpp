b485e6c29de31f6fb3b822cf264dcf6deb758769
Jim Verhaeghe <12163046+jpverhaeghe@users.noreply.github.com>
2023-03-21 06:53:13 +0000

    Add project files.

---
+++ Source/MinotaurEscapeUnreal/MinotaurEscapeUnreal.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MinotaurEscapeUnreal.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MinotaurEscapeUnreal, "MinotaurEscapeUnreal" );
---
+++ Source/MinotaurEscapeUnreal/MinotaurEscapeUnrealGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MinotaurEscapeUnrealGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MINOTAURESCAPEUNREAL_API AMinotaurEscapeUnrealGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
